9f72cd6d8d002fec917bcf35e5a80f0fb714631b
iprocousin <cusson@iprogroup.ca>
2021-09-06 20:01:49 -0700

    missing text

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import './App.css';
 function Footer() {
   return (
     <div className="footer">
-      &copy; {new Date().getFullYear()}, with &#10084; by <a href="https://cussoncheung.com" target="_blank">Cusson</a>.
+      &copy; {new Date().getFullYear()}, made with &#10084; by <a href="https://cussoncheung.com" target="_blank">Cusson</a>. <br />All rights reserved
     </div>
   );
 }
