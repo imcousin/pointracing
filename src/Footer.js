588abe51a965bf4f6e2725878f35440438bc2b0c
imcousin <cusson12@gmail.com>
2021-10-11 17:42:58 -0700

    new points

--- src/Footer.js
+++ src/Footer.js
@@ -4,6 +4,7 @@ function Footer() {
   return (
     <div className="footer">
       &copy; {new Date().getFullYear()} <a href="https://cussoncheung.com" target="_blank" rel="noopener noreferrer">Cusson</a>, all rights reserved. Made with &#10084;.
+      <p>Updated on Oct 11, 2021</p>
     </div>
   );
 }
